0f4794db54c270a7055ae0f6059b08d695954d95
Rebecca Dai <rhdai10@gmail.com>
2019-08-30 14:45:12 -0700

    subscription widget header

--- subscription-portal/widgets/personal-data/src/AppComponent.js
+++ subscription-portal/widgets/personal-data/src/AppComponent.js
@@ -15,12 +15,6 @@ export default class extends React.Component {
 					</div>
 				</div>
 				<div className="col-md-12 subscriptions row">
-					<div className="col-md-12 subscription-header">
-						<h6>Your Subscriptions</h6>
-
-						<button className="btn btn-link">Add</button>
-					</div>
-
 					<div className="col-md-4">
 						<Card heading="My Data" subheading="Premium Plus">
 							Chart
--- subscription-portal/widgets/recommendations/src/index.js
+++ subscription-portal/widgets/recommendations/src/index.js
@@ -7,24 +7,14 @@ import ErrorBoundary from './ErrorBoundary';
 /**
  * This is the main entry point of the portlet.
  *
- * See https://tinyurl.com/js-ext-portlet-entry-point for the most recent
- * information on the signature of this function.
  *
  * @param  {Object} params a hash with values of interest to the portlet
  * @return {void}
  */
-export default function main({
-	portletNamespace,
-	contextPath,
-	portletElementId
-}) {
+export default function main({portletElementId}) {
 	ReactDOM.render(
 		<ErrorBoundary>
-			<AppComponent
-				portletNamespace={portletNamespace}
-				contextPath={contextPath}
-				portletElementId={portletElementId}
-			/>
+			<AppComponent />
 		</ErrorBoundary>,
 		document.getElementById(portletElementId)
 	);
