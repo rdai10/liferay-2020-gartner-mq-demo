1c87c27d5fcb41e22dcb43429f7aad4306931cda
Rebecca Dai <rhdai10@gmail.com>
2019-09-04 16:41:24 -0700

    usage component

--- subscription-portal/widgets/usage/src/AppComponent.js
+++ subscription-portal/widgets/usage/src/AppComponent.js
@@ -1,8 +1,54 @@
 import React from 'react';
+
 import Card from './Card';
+import Modal from './Modal';
+
+// import img from '../assets/img/usage-car.png';
 
 export default class extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.handleDisplayModal = this.handleDisplayModal.bind(this);
+
+		this.state = {
+			showModal: false
+		};
+	}
+
+	handleDisplayModal() {
+		this.setState({
+			showModal: true
+		});
+	}
+
 	render() {
-		return <Card heading="Usage">Car</Card>;
+		const {showModal} = this.state;
+
+		return (
+			<Card heading="Usage">
+				<button
+					className="btn btn-link"
+					onClick={this.handleDisplayModal}
+					type="button"
+				>
+					<img
+						// Import img file as img tag src fails silently, hard coding url for now
+						src={
+							'http://localhost:8080/documents/20123/46507/usage-car+%281%29.png/9fb8f04b-2a07-1fa0-d162-18d8c24b632c?version=1.0&t=1567632736711&download=true'
+						}
+						alt="Usage Car Image"
+					/>
+
+					<Modal
+						onClose={this.handleCloseModal}
+						show={showModal}
+						size="full-screen"
+					>
+						Body
+					</Modal>
+				</button>
+			</Card>
+		);
 	}
 }
---
+++ subscription-portal/widgets/usage/src/Modal.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+class Modal extends React.Component {
+	render() {
+		const {children, footer, header, onClose, show, size} = this.props;
+
+		return (
+			show && (
+				<div className="modal show" role="dialog" tabIndex="-1">
+					<div className={`modal-dialog ${size ? `modal-${size}` : ''}`}>
+						<div className="modal-content">
+							{header && (
+								<div className="modal-header">
+									<div className="modal-title">{header}</div>
+
+									<button
+										aria-label="Close"
+										className="close"
+										onClick={onClose}
+										type="button"
+									>
+										X
+									</button>
+								</div>
+							)}
+
+							<div className="modal-body">{children}</div>
+
+							{footer && <div className="modal-footer">{footer}</div>}
+						</div>
+					</div>
+				</div>
+			)
+		);
+	}
+}
+
+export default Modal;
